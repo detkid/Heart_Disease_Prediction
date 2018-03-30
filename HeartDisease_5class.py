1823636ab87c94339da334bab7d539c4725044fc
yutaro ohira <detkid@gmail.com>
2018-03-31 01:36:50 +0900

    データ水増し　val_acc 50%

--- HeartDisease_5class.py
+++ HeartDisease_5class.py
@@ -7,7 +7,7 @@ from keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 
-batch_size = 100
+batch_size = 124
 num_classes = 5
 epochs = 2000
 
@@ -41,6 +41,118 @@ plt.hist(y_train)
 plt.hist(y_test)
 plt.show()
 
+# ラベル1のデータ水増し
+additional_rabel = 1
+multiplicity = 2
+rabel_index = np.where(y_train == additional_rabel)[0]
+
+newdata = np.zeros((rabel_index.shape[0], x_train.shape[1]))
+newdata_rabel = np.zeros(rabel_index.shape[0])
+
+for num in range(multiplicity-1):
+    for data_index in range(rabel_index.shape[0]):
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] += 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] -= 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        newdata_rabel[data_index] = additional_rabel
+    x_train = np.r_[x_train, newdata]
+    y_train = np.r_[y_train, newdata_rabel]
+
+# ラベル2のデータ水増し
+additional_rabel = 2
+multiplicity = 2
+rabel_index = np.where(y_train == additional_rabel)[0]
+
+newdata = np.zeros((rabel_index.shape[0], x_train.shape[1]))
+newdata_rabel = np.zeros(rabel_index.shape[0])
+
+for num in range(multiplicity-1):
+    for data_index in range(rabel_index.shape[0]):
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] += 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] -= 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        newdata_rabel[data_index] = additional_rabel
+    x_train = np.r_[x_train, newdata]
+    y_train = np.r_[y_train, newdata_rabel]
+
+# ラベル3のデータ水増し
+additional_rabel = 3
+multiplicity = 2
+rabel_index = np.where(y_train == additional_rabel)[0]
+
+newdata = np.zeros((rabel_index.shape[0], x_train.shape[1]))
+newdata_rabel = np.zeros(rabel_index.shape[0])
+
+for num in range(multiplicity-1):
+    for data_index in range(rabel_index.shape[0]):
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] += 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] -= 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        newdata_rabel[data_index] = additional_rabel
+    x_train = np.r_[x_train, newdata]
+    y_train = np.r_[y_train, newdata_rabel]
+
+# ラベル4のデータ水増し
+additional_rabel = 4
+multiplicity = 5
+rabel_index = np.where(y_train == additional_rabel)[0]
+
+newdata = np.zeros((rabel_index.shape[0], x_train.shape[1]))
+newdata_rabel = np.zeros(rabel_index.shape[0])
+
+for num in range(multiplicity-1):
+    for data_index in range(rabel_index.shape[0]):
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] += 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        for add_kaisu in range(10):
+            rand_int = np.random.randint(0, 13, 1)
+            if x_train[rabel_index[data_index], rand_int] != -9:
+                newdata[data_index] = x_train[rabel_index[data_index]]
+                newdata[data_index, rand_int] -= 0.01 * \
+                    x_train[rabel_index[data_index], rand_int]
+        newdata_rabel[data_index] = additional_rabel
+    x_train = np.r_[x_train, newdata]
+    y_train = np.r_[y_train, newdata_rabel]
+
+plt.hist(y_train)
+plt.hist(y_test)
+plt.show()
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -58,7 +170,8 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-tb_cb = keras.callbacks.TensorBoard(log_dir="log/tflog_0330_4class/", histogram_freq=1)
+tb_cb = keras.callbacks.TensorBoard(
+    log_dir="log/tflog_0330_5class/", histogram_freq=1)
 cbks = [tb_cb]
 
 history = model.fit(x_train, y_train,
